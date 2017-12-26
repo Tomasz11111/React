aaf6adb0067ad11cc21c71e6953cf913392943a7
Tomasz11111 <tomasz.dziendziura@gmail.com>
2017-12-26 17:26:42 +0100

    zadanie 6

--- script.js
+++ script.js
@@ -1,2 +1,56 @@
-var element = React.createElement('div', {}, 'Hello world!');
-ReactDOM.render(element, document.getElementById('app'));
+var Counter = React.createClass({
+	
+	getDefaultProps: function() {
+		console.log('Tutaj można zdefiniowac domyślne propsy')
+	},
+	
+	getInitialState: function() {
+		return {
+			counter: 0
+		};
+	},
+	increment: function() {
+		this.setState({
+			counter: this.state.counter +1
+		});
+	},
+	decrement: function() {
+		this.setState({
+			counter: this.state.counter -1
+		});
+	},
+	componentWillMount: function() {
+		console.log('Tutaj możemy coś zrobić, ale nie wiem co');
+	},
+	
+	render: function() {
+		return React.createElement('div', {className: 'countersContainer'},
+			React.createElement('div', {className: 'buttonInc', onClick: this.increment}, 'click here to increment' ),
+			React.createElement('div', {className: 'buttonDec', onClick: this.decrement}, 'click here to decrement'),
+			React.createElement('div', {}, 'Licznik: ' + this.state.counter)
+		);
+	},
+	componentDidMount: function() {
+		console.log('Tutaj można manipulowac elementem, który jest już w drzewie DOM, np. użyć jQuery')
+	},
+	//koniec fazy inicjalizacji
+	componentWillReceiveProps: function() {
+		console.log('aktualizowanie stanu');
+	},
+	shouldComponentUpdate : function() {
+		console.log('aktualizacja komponentu')
+		return true;
+	},
+	componentWillUpdate: function() {
+		console.log('??')
+	},
+	componentDidMount: function() {
+		console.log('Mozna wykonać kolejne operacje na drzewie DOM')
+	},
+	
+	
+});
+
+
+var app = React.createElement(Counter);
+ReactDOM.render(app, document.getElementById('app'));
