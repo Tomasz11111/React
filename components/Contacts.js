0ff95605ad6196c1b7f574dd21580552f3f2fddf
Tomasz11111 <tomasz.dziendziura@gmail.com>
2018-01-02 18:12:51 +0100

    uzupełniona składnia JSX w plikacji App.js i Contacts.js

--- components/Contacts.js
+++ components/Contacts.js
@@ -5,12 +5,16 @@ var Contacts = React.createClass({
 	
 	render: function() {
 		var contacts = this.props.items.map(function(contact) {
-			return React.createElement(Contact, {item: contact, key: contact.id});
-		});
-		
-		return (
-			React.createElement('ul', {className: 'contactsList'}, contacts)
+			return (
+				<Contact item={contact} key={contact.id}/>
 		);
-	}
+		});
+		return (
+			<ul className={'contactsList'}> 
+			{contacts}
+			</ul>
+		);
 	
+	
+}
 });
